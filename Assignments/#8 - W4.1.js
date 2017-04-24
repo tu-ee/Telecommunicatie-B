ab40f5849bfda9520071874d1142a14f9a6db0c2
EE <EE@EE.nl>
2017-04-24 16:21:06 +0200

    Add lower option for #8

--- Assignments/#8 - W4.1.js
+++ Assignments/#8 - W4.1.js
@@ -1,23 +1,25 @@
 // Question 1
-var low_frequency_in_khz = 24100;
-var high_frequency_in_khz = 48400;
-var if_frequency_in_khz = 3225;
-
+var low_frequency_in_khz = 24300;
+var high_frequency_in_khz = 39400;
+var higher_or_lower = 'lower'; // lower/higher
+var if_frequency_in_khz = 3395;
 
 
 require('./definitions.js');
 
-var oscilator_low = low_frequency_in_khz + if_frequency_in_khz;
+var factor = higher_or_lower === 'higher' ? 1 : -1;
+
+var oscilator_low = low_frequency_in_khz + factor * if_frequency_in_khz;
 oscilator_low *= kilo;
-var oscilator_high = high_frequency_in_khz + if_frequency_in_khz;
+var oscilator_high = high_frequency_in_khz + factor * if_frequency_in_khz;
 oscilator_high *= kilo;
 
 console.log(`1.a.1) ${oscilator_low/kilo} kHz`);
 console.log(`1.a.2) ${oscilator_high/kilo} kHz`);
 
-var image_low = low_frequency_in_khz + 2 * if_frequency_in_khz;
+var image_low = low_frequency_in_khz + factor * 2 * if_frequency_in_khz;
 image_low *= kilo;
-var image_high = high_frequency_in_khz + 2 * if_frequency_in_khz;
+var image_high = high_frequency_in_khz + factor * 2 * if_frequency_in_khz;
 image_high *= kilo;
 
 console.log(`1.b.1) ${image_low/kilo} kHz`);
